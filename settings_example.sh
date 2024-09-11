f68bb917594b59db35cb14819862d2e4d488979c
J-Donald Tournier <jdtournier@gmail.com>
2024-09-11 18:26:08 +0100

    A number of changes
    - default to C++20
    - include settings_example file
    - support folder structure
    - fix compilation when unused cpp files are present
    - don't run when multiple executables have been produced

---
+++ settings_example.sh
@@ -0,0 +1,23 @@
+# set compiler executable:
+cxx=g++
+
+# set default C++ language standard:
+std=c++20
+
+# default flags for compiling and linking:
+# these will be included in all builds (release or debug)
+cflags="-Wall"
+ldflags=""
+
+# default compiler flags used for optimised build:
+# these will be included in addition to cflags/ldflags
+cflags_optim="-O2 -DNDEBUG"
+ldflags_optim=""
+
+# default compiler flags used for debug build:
+# these will be included in addition to cflags/ldflags
+cflags_debug="-fsanitize=address,undefined -g"
+ldflags_debug="-fsanitize=address,undefined -g"
+
+# command to run to display compiler output on error:
+display_error="less -X"
