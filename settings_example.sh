3e892f3a01732e13f5fee7a4d8033b91a91fbfb3
J-Donald Tournier <jdournier@gmail.com>
2026-03-03 16:48:33 +0000

    remove unnecessary quotes in configs

--- settings_example.sh
+++ settings_example.sh
@@ -5,17 +5,17 @@ cxx=g++
 std=c++17
 
 # default flags for compiling and linking:
-cflags['release']="-Wall -O2 -DNDEBUG"
-ldflags['release']=""
+cflags[release]="-Wall -O2 -DNDEBUG"
+ldflags[release]=""
 
 # default compiler flags used for debug build:
-cflags['debug']="-Wall -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -g"
-ldflags['debug']="-g"
+cflags[debug]="-Wall -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -g"
+ldflags[debug]="-g"
 
 # example of additional build config if desired (delete if not necessary):
-description['assert']="create optimised build with asserts enabled"
-cflags['assert']="-Wall -O2 -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC"
-ldflags['assert']=""
+description[assert]="create optimised build with asserts enabled"
+cflags[assert]="-Wall -O2 -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC"
+ldflags[assert]=""
 
 # command to run to display compiler output on error:
 display_error="less -X"
